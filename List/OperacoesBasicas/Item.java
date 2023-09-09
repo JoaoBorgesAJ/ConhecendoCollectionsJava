c9a80f69ed366bd219470135ea48d3c7e970674c
João Paulo Borges da Silva <joao.borges@hugol.local>
2023-09-09 12:56:37 -0300

    Collections

---
+++ List/OperacoesBasicas/Item.java
@@ -0,0 +1,34 @@
+package List.OperacoesBasicas;
+
+public class Item {
+    // Atributos
+    private String nome;
+    private double preco;
+    private int quant;
+
+    public Item(String nome, double preco, int quant) {
+        this.nome = nome;
+        this.preco = preco;
+        this.quant = quant;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public int getQuant() {
+        return quant;
+    }
+
+    public String toString() {
+        return "Item{" +
+                "Produto: '" + nome + '\'' +
+                ", Preço R$ " + preco +
+                ", QTD: " + quant +
+                '}';
+    }
+}
